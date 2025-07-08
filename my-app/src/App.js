383dc0afd19a9ffdf715e17e849ccb0579907180
hosam3574 <hossamtradat@gmail.com>
2025-07-08 21:53:04 +0300

    react

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+import icon from''
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+      <FigmaNav/>
+      </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/componts/figma/figmaNav.js
@@ -0,0 +1,26 @@
+import FigmaNav from'./figma/FigmaNav'
+
+export default function FigmaNav (){
+
+
+return(
+<div>
+
+
+
+<h1>gfkhj</h1>
+</div>
+
+ 
+
+
+
+
+
+
+
+)
+
+
+
+}
